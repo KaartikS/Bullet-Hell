8d0a3ddffc6847006f53dee054e5008564db9d70
Benji <kaartik.servansingh@gmail.com>
2023-06-11 08:09:46 -0700

    Player Animation
    Basic movement animations and flipping of sprites based on mouse relative position to player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,12 +10,15 @@ public class PlayerController : MonoBehaviour
     private PlayerControls playerControls;
     private Vector2 movement;
     private Rigidbody2D rb;
-
+    private Animator playerAnimator;
+    private SpriteRenderer playerSpriteRenderer;
 
     private void Awake()
     {
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+        playerSpriteRenderer = GetComponent<SpriteRenderer>();
         
     }
 
@@ -32,16 +35,36 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        AdjustPlayerFacingDirection();
         Move();
     }
 
     private void PlayerInput()
     {
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
+        playerAnimator.SetFloat("moveX", movement.x);
+        playerAnimator.SetFloat("moveY", movement.y);
     }
 
     private void Move()
     {
         rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+
+    private void AdjustPlayerFacingDirection()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        if(mousePosition.x < playerScreenPoint.x)
+        {
+            // flip player sprites
+            playerSpriteRenderer.flipX = true;
+        }
+        else
+        {
+            playerSpriteRenderer.flipX = false;
+        }
+
     }
 }
